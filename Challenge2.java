1826d2dca8262455296c68206af85825f184c198
ajrnnn <muriithia05@gmail.com>
2025-04-28 11:47:54 +0300

    Challenge2

--- Challenge2.java
+++ Challenge2.java
@@ -1,6 +1,6 @@
 public class Challenge2 {
 public static void main(String []args){
-   int birthdate = 2005;
+   int birthdate = 2006;
    int currentyear = 2025;
 
    int age = currentyear - birthdate;
