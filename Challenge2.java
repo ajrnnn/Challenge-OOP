eab04c498c6548d5c0637a32a2ed4355dad5f264
ajrnnn <muriithia05@gmail.com>
2025-04-28 11:47:10 +0300

    Add files via upload

---
+++ Challenge2.java
@@ -0,0 +1,10 @@
+public class Challenge2 {
+public static void main(String []args){
+   int birthdate = 2005;
+   int currentyear = 2025;
+
+   int age = currentyear - birthdate;
+
+   System.out.println("You are: " +age);
+}
+}
