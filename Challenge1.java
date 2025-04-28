254f279002c0f58376444fbcfb2cede4f275a2fd
ajrnnn <muriithia05@gmail.com>
2025-04-28 11:50:02 +0300

    Add files via upload

---
+++ Challenge1.java
@@ -0,0 +1,10 @@
+public class Challenge1 {
+    void greetings(){
+        System.out.println("How are you?");
+
+    }
+    public static void main(String [] arg){
+        Challenge1 obj = new Challenge1();
+                obj.greetings();
+    }
+}
