d0ea45a0b6c7ca997c8c2ec64d43a034a5f798e8
ajrnnn <muriithia05@gmail.com>
2025-04-28 11:50:26 +0300

    Challenge1.java

--- Challenge1.java
+++ Challenge1.java
@@ -1,6 +1,6 @@
 public class Challenge1 {
     void greetings(){
-        System.out.println("How are you?");
+        System.out.println("How are you? ");
 
     }
     public static void main(String [] arg){
